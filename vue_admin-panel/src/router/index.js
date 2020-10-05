77c1a8ed968816773f155a2fd932009e11716a79
megiakhalkatsi <megi.akhalkatsi@gmail.com>
2020-10-05 13:33:39 +0400

    update router / admin panel

--- vue_admin-panel/src/router/index.js
+++ vue_admin-panel/src/router/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Dashboard from '../views/Dashboard.vue'
+import Statistics from '../views/Statistics.vue'
 import MyWords from '../views/MyWords.vue'
 import Words from '../views/Words.vue'
-import Profile from '../views/Profile.vue'
+import Users from '../views/Users.vue'
+import Administration from '../views/Administration.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'dashboard',
-    component: Dashboard
+    name: 'statistics',
+    component: Statistics
   },
   {
     path: '/myWords',
@@ -25,9 +26,14 @@ const routes = [
     component: Words
   },
   {
-    path: '/profile',
-    name: 'profile',
-    component: Profile
+    path: '/users',
+    name: 'users',
+    component: Users
+  },
+  {
+    path: '/administration',
+    name: 'administration',
+    component: Administration
   }
 ]
 
--- vue_admin-panel/src/views/Statistics.vue
+++ vue_admin-panel/src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="dashboard">
-    <h1>Dashboard</h1>
+  <div class="Statistics">
+    <h1>Statistics</h1>
   </div>
 </template>
 
