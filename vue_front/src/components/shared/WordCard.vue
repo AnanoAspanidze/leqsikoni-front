cbf8aa4ef19f751de01bf9126bc2ccbb8a0319b2
Rome00 <r.khazalia@gmail.com>
2020-11-21 12:34:32 +0400

    🎨 word section restructured

--- vue_front/src/components/shared/WordCard.vue
+++ vue_front/src/components/shared/WordCard.vue
@@ -6,63 +6,201 @@
       :class="[!$screen.tablet ? 'mx-3' : 'mx-6']"
     >
       <slot name="edit"></slot>
-      <!-- fist content -->
       <div class="card-content_line is-flex is-flex-direction-column">
-        <div
-          v-for="word in words.itemsList"
-          :key="word.itemId"
-          class="is-flex mb-3"
-        >
-          <div
-            class="card-content_line-icon has-background-warning is-flex is-justify-content-center px-1 py-1"
-          >
-            <b-tooltip
-              :label="word.user.userName"
-              :triggers="['click']"
-              position="is-bottom"
-              type="is-info"
-              :auto-close="['outside', 'escape']"
+        <!-- English content -->
+        <div :class="{ 'mb-3': $route.name === 'SingleWord' }">
+          <div v-for="word in engWords" :key="word.itemId" class="is-flex mt-3">
+            <div
+              class="card-content_line-icon has-background-warning is-flex is-justify-content-center px-1 py-1"
             >
-              <b-icon
-                size="is-small"
-                icon="user"
-                type="is-white"
-                class="is-clickable"
-              ></b-icon>
-            </b-tooltip>
-          </div>
-          <div
-            class="card-content_line-icon has-background-primary is-inline-flex is-justify-content-center px-1 py-1 ml-2"
-          >
-            <b-tooltip
-              :triggers="['click']"
-              position="is-bottom"
-              type="is-info"
-              :auto-close="['outside', 'escape']"
+              <b-tooltip
+                :label="word.user.userName"
+                :triggers="['click']"
+                position="is-bottom"
+                type="is-info"
+                :auto-close="['outside', 'escape']"
+              >
+                <b-icon
+                  size="is-small"
+                  icon="user"
+                  type="is-white"
+                  class="is-clickable"
+                ></b-icon>
+              </b-tooltip>
+            </div>
+            <div
+              class="card-content_line-icon has-background-primary is-inline-flex is-justify-content-center px-1 py-1 ml-2"
             >
-              <template v-slot:content>
-                <div class="is-flex is-flex-direction-column">
-                  <p class="pb-1">წყარო: {{ word.sourceText }}</p>
-                  <a class="pt-1" href="word.sourceLink">ლინკი</a>
-                </div>
-              </template>
-              <b-icon
-                size="is-small"
-                icon="info"
-                type="is-white"
-                class="is-clickable"
-              ></b-icon>
-            </b-tooltip>
+              <b-tooltip
+                :triggers="['click']"
+                position="is-bottom"
+                type="is-info"
+                :auto-close="['outside', 'escape']"
+              >
+                <template v-slot:content>
+                  <div class="is-flex is-flex-direction-column">
+                    <p class="pb-1">წყარო: {{ word.sourceText }}</p>
+                    <a class="pt-1" href="word.sourceLink">ლინკი</a>
+                  </div>
+                </template>
+                <b-icon
+                  size="is-small"
+                  icon="info"
+                  type="is-white"
+                  class="is-clickable"
+                ></b-icon>
+              </b-tooltip>
+            </div>
+            <slot name="icon"></slot>
+            <p
+              v-if="word.wordType.toLowerCase() === 'eng'"
+              class="has-text-primary is-family-secondary has-text-weight-bold is-capitalized ml-3"
+            >
+              {{ word.wordName }}
+            </p>
+            <p
+              v-if="word.wordType.toLowerCase() === 'geo'"
+              class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3 mt"
+            >
+              {{ word.wordName }}
+            </p>
+            <p
+              v-if="word.wordType.toLowerCase() === 'def'"
+              class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3"
+            >
+              {{ word.wordName }}
+            </p>
           </div>
-          <slot name="icon"></slot>
-          <p
-            class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3"
-            :class="{
-              'has-text-primary': word.wordType.toLowerCase() === 'eng'
-            }"
-          >
-            {{ word.wordName }}
-          </p>
+        </div>
+        <!-- Georgian content -->
+        <div :class="{ 'my-3': $route.name === 'SingleWord' }">
+          <div v-for="word in geoWords" :key="word.itemId" class="is-flex mt-3">
+            <div
+              class="card-content_line-icon has-background-warning is-flex is-justify-content-center px-1 py-1"
+            >
+              <b-tooltip
+                :label="word.user.userName"
+                :triggers="['click']"
+                position="is-bottom"
+                type="is-info"
+                :auto-close="['outside', 'escape']"
+              >
+                <b-icon
+                  size="is-small"
+                  icon="user"
+                  type="is-white"
+                  class="is-clickable"
+                ></b-icon>
+              </b-tooltip>
+            </div>
+            <div
+              class="card-content_line-icon has-background-primary is-inline-flex is-justify-content-center px-1 py-1 ml-2"
+            >
+              <b-tooltip
+                :triggers="['click']"
+                position="is-bottom"
+                type="is-info"
+                :auto-close="['outside', 'escape']"
+              >
+                <template v-slot:content>
+                  <div class="is-flex is-flex-direction-column">
+                    <p class="pb-1">წყარო: {{ word.sourceText }}</p>
+                    <a class="pt-1" href="word.sourceLink">ლინკი</a>
+                  </div>
+                </template>
+                <b-icon
+                  size="is-small"
+                  icon="info"
+                  type="is-white"
+                  class="is-clickable"
+                ></b-icon>
+              </b-tooltip>
+            </div>
+            <slot name="icon"></slot>
+            <p
+              v-if="word.wordType.toLowerCase() === 'eng'"
+              class="has-text-primary is-family-secondary has-text-weight-bold is-capitalized ml-3"
+            >
+              {{ word.wordName }}
+            </p>
+            <p
+              v-if="word.wordType.toLowerCase() === 'geo'"
+              class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3 mt"
+            >
+              {{ word.wordName }}
+            </p>
+            <p
+              v-if="word.wordType.toLowerCase() === 'def'"
+              class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3"
+            >
+              {{ word.wordName }}
+            </p>
+          </div>
+        </div>
+        <!-- Definition content -->
+        <div :class="{ 'mt-3': $route.name === 'SingleWord' }">
+          <div v-for="word in defWords" :key="word.itemId" class="is-flex mt-3">
+            <div
+              class="card-content_line-icon has-background-warning is-flex is-justify-content-center px-1 py-1"
+            >
+              <b-tooltip
+                :label="word.user.userName"
+                :triggers="['click']"
+                position="is-bottom"
+                type="is-info"
+                :auto-close="['outside', 'escape']"
+              >
+                <b-icon
+                  size="is-small"
+                  icon="user"
+                  type="is-white"
+                  class="is-clickable"
+                ></b-icon>
+              </b-tooltip>
+            </div>
+            <div
+              class="card-content_line-icon has-background-primary is-inline-flex is-justify-content-center px-1 py-1 ml-2"
+            >
+              <b-tooltip
+                :triggers="['click']"
+                position="is-bottom"
+                type="is-info"
+                :auto-close="['outside', 'escape']"
+              >
+                <template v-slot:content>
+                  <div class="is-flex is-flex-direction-column">
+                    <p class="pb-1">წყარო: {{ word.sourceText }}</p>
+                    <a class="pt-1" href="word.sourceLink">ლინკი</a>
+                  </div>
+                </template>
+                <b-icon
+                  size="is-small"
+                  icon="info"
+                  type="is-white"
+                  class="is-clickable"
+                ></b-icon>
+              </b-tooltip>
+            </div>
+            <slot name="icon"></slot>
+            <p
+              v-if="word.wordType.toLowerCase() === 'eng'"
+              class="has-text-primary is-family-secondary has-text-weight-bold is-capitalized ml-3"
+            >
+              {{ word.wordName }}
+            </p>
+            <p
+              v-if="word.wordType.toLowerCase() === 'geo'"
+              class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3 mt"
+            >
+              {{ word.wordName }}
+            </p>
+            <p
+              v-if="word.wordType.toLowerCase() === 'def'"
+              class="has-text-text is-family-secondary has-text-weight-bold is-capitalized ml-3"
+            >
+              {{ word.wordName }}
+            </p>
+          </div>
         </div>
       </div>
 
@@ -91,6 +229,23 @@
       words: {
         type: [Object, Number],
         required: true
+      }
+    },
+    computed: {
+      engWords() {
+        return this.words.itemsList.filter(
+          x => x.wordType.toLowerCase() === 'eng'
+        )
+      },
+      geoWords() {
+        return this.words.itemsList.filter(
+          x => x.wordType.toLowerCase() === 'geo'
+        )
+      },
+      defWords() {
+        return this.words.itemsList.filter(
+          x => x.wordType.toLowerCase() === 'def'
+        )
       }
     }
   }
